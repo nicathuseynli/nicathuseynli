567ce8d18a71b200d0f66d44fb0390dacb031add
nicathuseynli <nijatkhh@code.edu.az>
2022-08-12 13:58:40 +0400

    homework4

---
+++ Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+
+namespace homework_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task 1
+            /*DateTime dateTime = new DateTime(2004, 06, 15);
+            Console.WriteLine(dateTime.DayOfWeek);*/
+            #endregion
+            #region task 2
+            /* DateTime dateTime = new DateTime(2004, 06, 15);
+             DateTime dateTime1 = new DateTime(2022, 09, 08);
+             TimeSpan time = dateTime1.Subtract(dateTime);
+             Console.WriteLine(time);*/
+            #endregion
+            #region task 3
+            /* Console.WriteLine("Hansının böyük olduğunu müəyyən etmək üçün lazım olan ededleri daxil edin:");
+             int a, b, c;
+             Console.Write("Ilk eded: ");
+             a = Convert.ToInt32(Console.ReadLine());
+             Console.Write("Ikinci eded: ");
+             b = Convert.ToInt32(Console.ReadLine());
+             Console.Write("Ucuncu eded: ");
+             c = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("=======================");
+             if(a > b)
+             {
+                 Console.WriteLine("Ilk eded boyukdurse ");
+                 if (a < b)
+                 {
+                     Console.WriteLine("Ikinci eded boyukdurse");
+                 }
+             }
+             if (a==b&&b==a&&c==a&&a==c)
+             {
+                 Console.WriteLine("Butun ededler eynidir");
+             }
+             else if (a < c)
+             {
+
+                 Console.WriteLine("Ucuncu eded boyuk olarsa");
+             }
+             else
+             {
+                 Console.WriteLine("Ikinci eded boyuk olarsa ");
+             }*/
+            #endregion
+            #region task 4
+            /*Console.WriteLine("Days of the week");
+            string text = Console.ReadLine();
+            int age = int.Parse(text);
+            switch (age)
+            {
+                case 1:
+                    Console.WriteLine("Bazar ertesi");
+                    break;
+                case 2:
+                    Console.WriteLine("Cersenbe axsami");
+                    break;
+                case 3:
+                    Console.WriteLine("Cersenbe");
+                    break;
+                case 4:
+                    Console.WriteLine("Cume axsami");
+                    break;
+                case 5:
+                    Console.WriteLine("Cume");
+                    break;
+                case 6:
+                    Console.WriteLine("Senbe");
+                    break;
+                case 7:
+                    Console.WriteLine("Bazar");
+                    break;
+                default:
+                    Console.WriteLine("Heftenin hec bir gunu");
+                    break;
+            }*/
+            #endregion
+            #region task 5
+            //Console.WriteLine("Ilk eded: ");
+            //string str_a = Console.ReadLine();
+            //int int_a = int.Parse(str_a);
+            //Console.WriteLine("Ikinci eded: ");
+            //string str_b = Console.ReadLine();
+            //int int_b =int.Parse(str_b);
+            //Console.WriteLine("1+  ,2-  ,3*  ,4/");
+            //string str_d = Console.ReadLine();
+            //switch (str_d) 
+            //{
+            //    case "1":
+            //        Console.WriteLine(Plus(int_a, int_b));
+            //        break;
+            //    case "2":
+            //        Console.WriteLine(Minus(int_a, int_b));
+            //        break;
+            //    case "3":
+            //        Console.WriteLine(Multiplication(int_a, int_b));
+            //        break;
+            //    case "4":
+            //        Console.WriteLine(Division(int_a, int_b));
+            //        break;
+            //}
+            #region task6
+            //int num, r, sum = 0, t;
+
+
+            //Console.Write("\n\n");
+            //Console.Write("Check whether a number is a palindrome or not:\n");
+            //Console.Write("------------------------------------------------");
+            //Console.Write("\n\n");
+
+            //Console.Write("Input a number: ");
+            //num = Convert.ToInt32(Console.ReadLine());
+
+            //for (t = num; num != 0; num = num / 10)
+            //{
+            //    r = num % 10;
+            //    sum = sum * 10 + r;
+            //}
+            //if (t == sum)
+            //    Console.Write("{0} is a palindrome number.\n", t);
+            //else
+            //    Console.Write("{0} is not a palindrome number.\n", t);
+            #endregion
+            #region task7
+            //{
+            //    public static int SumCal(int n)
+            //    {
+            //        string n1 = Convert.ToString(n);
+            //        int sum = 0;
+            //        for (int i = 0; i < n1.Length; i++)
+            //            sum += Convert.ToInt32(n1.Substring(i, 1));
+            //        return sum;
+            //    }
+
+            //    public static void Main()
+            //    {
+            //        int num;
+            //        Console.Write("\n\nFunction : To calculate the sum of the individual digits of a number :\n");
+            //        Console.Write("--------------------------------------------------------------------------\n");
+            //        Console.Write("Enter a number: ");
+            //        num = Convert.ToInt32(Console.ReadLine());
+            //        Console.WriteLine("The sum of the digits of the number {0} is : {1} \n", num, SumCal(num));
+            //    }
+            //}
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
